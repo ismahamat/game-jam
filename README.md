3cbc5f2447e2da767f525ada4d5e1f4873a2f700
ismahamat <issa.mahamatissa@edu.esiee.fr>
2025-09-16 14:12:18 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # OUT OF SCALE
 
 Dans `Out of scale`, vous n'êtes pas le centre du monde.
+
+## Lancer le projet ##
+
+- Se mettre à la racine du projet;
+- Creer un environnement virtuel; 
+- 
---
+++ main.py
@@ -0,0 +1,37 @@
+import arcade
+
+# Dimensions de la fenêtre
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Mon premier jeu Arcade"
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        """Initialisation du jeu"""
+        pass
+
+    def on_draw(self):
+        """Affichage des éléments"""
+        self.clear()
+        arcade.draw_text("Hello Arcade!", 100, 300, arcade.color.WHITE, 24)
+
+    def on_update(self, delta_time):
+        """Logique du jeu (appelé 60 fois/sec)"""
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """Quand une touche est pressée"""
+        if key == arcade.key.SPACE:
+            print("Espace pressé !")
+
+def main():
+    game = MyGame()
+    game.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
