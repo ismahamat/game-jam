7c80e17d4db4d6348687b6cfaa9b814a9c835fdf
Lirido1404 <maxime.prevot1804kz@gmail.com>
2025-09-17 11:57:11 +0200

    atoms2

--- scenes/atom.py
+++ scenes/atom.py
@@ -1,115 +1,179 @@
 import arcade
 from .base import BaseView
 
-TILE_SCALING = 0.5
 PLAYER_MOVEMENT_SPEED = 5
-GRAVITY = 1
-PLAYER_JUMP_SPEED = 20
-
+TILE_SCALING = 0.5
+scale = 0.3
 
 class AtomView(BaseView):
     def __init__(self):
         super().__init__()
 
-        self.player_texture = None
-        self.player_sprite = None
-
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-
-        self.physics_engine = None
-        self.camera = None
+        self.player_list = arcade.SpriteList()         # Les atomes principaux
+        self.bigatom_list = arcade.SpriteList()       # Le gros noyau
+        self.atoms_exemple_list = arcade.SpriteList() # Mini-atomes en haut à droite
 
 
+        # Fond
+        self.background_color = arcade.color.BLACK
 
+        # Index de l'atome actuellement contrôlé
+        self.current_atom_index = 0
+        self.background_list = arcade.SpriteList()  # Liste pour le fond
+
+
+        # Touches pressées
+        self.keys_held = {"up": False, "down": False, "left": False, "right": False}
 
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-        self.player_texture = arcade.load_texture(
-            "images/test.png"
-        )
-        
-        self.camera = arcade.Camera2D()
-        
-
-        self.player_sprite = arcade.Sprite(self.player_texture)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
-        
+        """Créer les atomes et l'UI"""
         self.player_list = arcade.SpriteList()
-        self.player_list.append(self.player_sprite)
+        atoms_coordinates = [[300,100],[500,200],[800,400],[450,630]]
         
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.wall_list.append(wall)
         
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite(
-                ":resources:images/tiles/boxCrate_double.png", scale=TILE_SCALING)
-            wall.position = coordinate
-            self.wall_list.append(wall)
-            
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite, walls=self.wall_list, gravity_constant=GRAVITY
-        )
+        self.background_list = arcade.SpriteList()  # Liste pour le fond
+        background = arcade.Sprite("images/bgimg.png")
+        background.center_x = self.window.width/2
+        background.center_y = self.window.height/2   
+        background.width = self.window.width
+        background.height = self.window.height
+        self.background_list.append(background)    
         
-        self.background_color = arcade.color.WHITE
+        atom_images = ["images/atome_1.png", "images/atome_2.png", "images/atome_3.png", "images/atome_4.png"]
 
+        # Atomes principaux
+        for i, img_path in enumerate(atom_images):
+            atom = arcade.Sprite(img_path, scale=TILE_SCALING)  # scale si besoin
+            atom.center_x = atoms_coordinates[i][0]
+            atom.center_y = atoms_coordinates[i][1]
+            self.player_list.append(atom)
 
+        # Gros atome (noyau)
+        self.bigatom_list = arcade.SpriteList()
+        bigatom = arcade.Sprite("images/atome_centre.png",scale=TILE_SCALING)
+        bigatom.center_x = self.window.width/2 - 2
+        bigatom.center_y = self.window.height/2 - 2
+        self.bigatom_list.append(bigatom)
+
+        # Mini-atoms en haut à droite
+        self.atoms_exemple_list = arcade.SpriteList()
+        for i, img_path in enumerate(atom_images):
+            scale = 0.3 if i == self.current_atom_index else 0.15 # Plus grand pour l'atome actif
+            atomex = arcade.Sprite(img_path, scale=scale)
+            atomex.center_x = self.window.width - 150 + i*8
+            atomex.center_y = self.window.height - 37
+            self.atoms_exemple_list.append(atomex)
 
     def on_draw(self):
         super().on_draw()
-        self.show_text_center("Atom - Prototype")
 
-        self.camera.use()
 
+        # Dessiner tous les atomes
+        self.background_list.draw()
         self.player_list.draw()
-        self.wall_list.draw()
-        
-        
+        self.bigatom_list.draw()
+        self.atoms_exemple_list.draw()
 
-        arcade.draw_rect_outline(arcade.rect.XYWH(60, self.window.height - 30,   # x, y du centre
+        # UI fixe en haut à gauche
+        arcade.draw_rect_outline(arcade.rect.XYWH(120, self.window.height - 30,   # x, y du centre
         200, 40,),
-                         arcade.color.BRITISH_RACING_GREEN)
+                         arcade.color.WHITE)
     
         arcade.draw_text(
             "Echelle : ",
-            30, self.window.height - 30,
-            arcade.color.BLACK, 14,
+            70, self.window.height - 30,
+            arcade.color.WHITE, 14,
             anchor_x="center", anchor_y="center"
         )
         
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed."""
+        #orbits
+        arcade.draw_circle_outline(self.window.width/2, self.window.height/2, 130, arcade.color.YELLOW, 3)
+        arcade.draw_circle_outline(self.window.width/2, self.window.height/2, 180, arcade.color.BLUE, 3)
+        arcade.draw_circle_outline(self.window.width/2, self.window.height/2, 230, arcade.color.ORANGE, 3)
+        arcade.draw_circle_outline(self.window.width/2, self.window.height/2, 280, arcade.color.PURPLE, 3)
 
-        if key == arcade.key.ESCAPE:
+
+
+
+        
+        #UI Fixe haut droite
+        arcade.draw_rect_outline(arcade.rect.XYWH(self.window.width-150, self.window.height - 30,   # x, y du centre
+        200, 40,),
+                         arcade.color.WHITE)
+        
+        arcade.draw_rect_outline(arcade.rect.XYWH(self.window.width-220, self.window.height - 30,   # x, y du centre
+        40, 25,),
+                         arcade.color.WHITE)
+    
+        arcade.draw_text(
+            "Tab",
+            self.window.width-220, self.window.height - 30,
+            arcade.color.WHITE, 14,
+            anchor_x="center", anchor_y="center"
+        )
+        
+        arcade.draw_text(
+            "Pour changer de POV",
+            self.window.width-130, self.window.height - 20,
+            arcade.color.WHITE, 7,
+            anchor_x="center", anchor_y="center"
+        )
+        
+        
+        # Indiquer quel atome est actif
+        active_atom = self.player_list[self.current_atom_index]
+        arcade.draw_circle_outline(active_atom.center_x, active_atom.center_y, 20, arcade.color.BLACK, 2)
+
+    def on_key_press(self, key, modifiers):
+        if key in (arcade.key.UP, arcade.key.W):
+            self.keys_held["up"] = True
+        elif key in (arcade.key.DOWN, arcade.key.S):
+            self.keys_held["down"] = True
+        elif key in (arcade.key.LEFT, arcade.key.A):
+            self.keys_held["left"] = True
+        elif key in (arcade.key.RIGHT, arcade.key.D):
+            self.keys_held["right"] = True
+        elif key == arcade.key.TAB:
+            # Passer à l'atome suivant
+            self.current_atom_index = (self.current_atom_index + 1) % len(self.player_list)
+        elif key == arcade.key.ESCAPE:
             self.setup()
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
-
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
-
     def on_key_release(self, key, modifiers):
-        """Called whenever a key is released."""
+        if key in (arcade.key.UP, arcade.key.W):
+            self.keys_held["up"] = False
+        elif key in (arcade.key.DOWN, arcade.key.S):
+            self.keys_held["down"] = False
+        elif key in (arcade.key.LEFT, arcade.key.A):
+            self.keys_held["left"] = False
+        elif key in (arcade.key.RIGHT, arcade.key.D):
+            self.keys_held["right"] = False
 
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
-            
     def on_update(self, delta_time):
-        """Movement and Game Logic"""
-        self.physics_engine.update()
-        self.camera.position = self.player_sprite.position
+        """Déplacer uniquement l'atome actif"""
+        dx = dy = 0
+        speed = PLAYER_MOVEMENT_SPEED
 
+        if self.keys_held["up"]:
+            dy += speed
+        if self.keys_held["down"]:
+            dy -= speed
+        if self.keys_held["left"]:
+            dx -= speed
+        if self.keys_held["right"]:
+            dx += speed
 
+        # Déplacer l'atome actif
+        active_atom = self.player_list[self.current_atom_index]
+        active_atom.center_x += dx
+        active_atom.center_y += dy
+
+        # Mettre à jour la mini-vue des atomes pour montrer l'actif
+        atom_images = ["images/atome_1.png", "images/atome_2.png", "images/atome_3.png", "images/atome_4.png"]
+
+        for i, img_path in enumerate(atom_images):
+            scale = 0.3 if i == self.current_atom_index else 0.15  # Plus grand pour l'atome actif
+            atomex = arcade.Sprite(img_path, scale=scale)
+            atomex.center_x = self.window.width - 150 + i*8
+            atomex.center_y = self.window.height - 37
+            self.atoms_exemple_list[i] = atomex
